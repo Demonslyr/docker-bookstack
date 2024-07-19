f07dc986c2ad1ee497db6eed0c4a8adacccbea80
Demonslyr <Demonslyr@users.noreply.github.com>
2024-07-19 05:20:21 -0500

    Update BuildBookstack.groovy

--- Jenkins/BuildBookstack.groovy
+++ Jenkins/BuildBookstack.groovy
@@ -1,29 +1,34 @@
 properties([pipelineTriggers([githubPush()])])
 node {
     jenkinsGitCredentials = 'jenkinsGitHubSvc'
-    git url: 'https://github.com/Demonslyr/docker-bookstack',branch: 'atriarch-k8s',credentialsId: jenkinsGitCredentials
+    git url: 'https://github.com/Demonslyr/Atriarch.Cdn.Client.git',branch: 'main',credentialsId: jenkinsGitCredentials
     stage('setup'){
         checkout scm
         currentBuild.description = "${Branch}"
         appName = "${ImageName}"
         dockerRepo = "${DockerRepo}"
         dockerCredId = "${DockerCredentials}"
-        dockerfilePathFromRoot = "${DockerFilePathAndFilename}"// this is the path from the base directory
-    }
-    stage('test'){
-        // def results = sh returnStdout: true, script: "dotnet test "
+        dockerfilePathFromRoot = "${DockerFilePathAndFilename}" // this is the path from the base directory
+        fullImageName = "${dockerRepo}/${appName}:v1.0.${BUILD_NUMBER}"
+        gitOpsRepo = "${GitOpsRepo}"
+        gitOpsBranch = "${GitOpsBranch}"
+        pathToDeploymentYaml = "${PathToDeploymentYaml}"
     }
     stage('build'){
-       def buildout = sh(returnStdout: true, script: "docker build -t ${appName} -f ${dockerfilePathFromRoot} .")
-       println buildout
+       withCredentials([usernamePassword(usernameVariable: "DOCKER_USER",passwordVariable: "DOCKER_PASS", credentialsId: dockerCredId)]){
+            def loginout = sh(returnStdout: true, script: "echo ${DOCKER_PASS} | docker login ${dockerRepo} --username ${DOCKER_USER} --password-stdin")
+            println loginout
+            def buildout = sh(returnStdout: true, script: "docker build -t ${appName} -f ${dockerfilePathFromRoot} .")
+            println buildout
+        }
     }
     stage('push'){
-        def tagout = sh(returnStdout: true, script: "docker tag ${appName} ${dockerRepo}/${appName}:latest")
+        def tagout = sh(returnStdout: true, script: "docker tag ${appName} ${fullImageName}")
         println tagout
         withCredentials([usernamePassword(usernameVariable: "DOCKER_USER",passwordVariable: "DOCKER_PASS", credentialsId: dockerCredId)]){
             def loginout = sh(returnStdout: true, script: "echo ${DOCKER_PASS} | docker login ${dockerRepo} --username ${DOCKER_USER} --password-stdin")
             println loginout
-            def pushout = sh(returnStdout: true, script: "docker push ${dockerRepo}/${appName}:latest")
+            def pushout = sh(returnStdout: true, script: "docker push ${fullImageName}")
             println pushout
         }
     }
